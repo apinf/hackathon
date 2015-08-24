1858cc375964e80db6687459da08f6fe28918277
elnzv <illya.nizyev@gmail.com>
2015-08-24 05:47:24 +0300

    built email

--- server/methods/registerForm.js
+++ server/methods/registerForm.js
@@ -4,9 +4,14 @@ Meteor.methods({
     check(doc, Schema.registerForm);
 
     // Build the e-mail text
-    var text = "Name: " + doc.name + "\n\n"
-    + "Email: " + doc.email + "\n\n\n\n"
-    + doc.message;
+    var text = "Name: " + doc.firstName + doc.secondName + "\n\n"
+    + "Email: " + doc.email + "\n\n"
+    + "Company / Organization / Educational institution: " + doc.company + "\n\n"
+    + "Special diet: " + doc.diet + "\n\n"
+    + "Other team members: " + doc.teamMembers + "\n\n"
+    + "Name of the hackathon team: " + doc.teamName + "\n\n"
+    + "Tell us about your team: " + doc.teamInfo + "\n\n"
+    + "Tell us about the type of problem you want to solve or idea you have: " + doc.teamIdeas + "\n\n";
 
     this.unblock();
 
@@ -14,7 +19,7 @@ Meteor.methods({
     Email.send({
       to: Meteor.settings.mail.masterEmail,
       from: doc.email,
-      subject: "Apinf Hackathon Register Form - Message From " + doc.name,
+      subject: "Apinf Hackathon Register Form - Message From " + doc.firstName + doc.secondName,
       text: text
     });
   }

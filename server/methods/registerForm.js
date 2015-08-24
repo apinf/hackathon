03d821b7770cd6e7d72053de9ab003373d599374
elnzv <illya.nizyev@gmail.com>
2015-08-24 03:28:44 +0300

    method to send emails

---
+++ server/methods/registerForm.js
@@ -0,0 +1,21 @@
+Meteor.methods({
+  register: function(doc) {
+    // Important server-side check for security and data integrity
+    check(doc, Schema.registerForm);
+
+    // Build the e-mail text
+    var text = "Name: " + doc.name + "\n\n"
+    + "Email: " + doc.email + "\n\n\n\n"
+    + doc.message;
+
+    this.unblock();
+
+    // Send the e-mail
+    Email.send({
+      to: "illya.nizyev@gmail.com",
+      from: doc.email,
+      subject: "Apinf Hackathon Register Form - Message From " + doc.name,
+      text: text
+    });
+  }
+});

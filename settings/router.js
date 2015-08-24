b2b4a5e40ac76f765e822828355ea5a3eb5df891
elnzv <illya.nizyev@gmail.com>
2015-08-24 13:58:15 +0300

    added a route for registrants

--- settings/router.js
+++ settings/router.js
@@ -5,3 +5,7 @@ Router.route('/', function () {
 Router.route('/register', function () {
   this.render('register');
 });
+
+Router.route('/registrants', function () {
+  this.render('registrants');
+});

c9c0d8ab84d186bb37f60b6a7b7e0040e20dbf9c
elnzv <illya.nizyev@gmail.com>
2015-08-24 03:29:22 +0300

    moved schema

---
+++ both/registerForm.js
@@ -0,0 +1,90 @@
+Schema = {};
+
+Schema.registerForm = new SimpleSchema({
+  firstName: {
+    type: String,
+    label: "First name",
+    max: 50,
+    optional: false,
+    autoform: {
+      placeholder: 'Your first name'
+    }
+  },
+  secondName: {
+    type: String,
+    label: "Second name",
+    max: 50,
+    optional: false,
+    autoform: {
+      placeholder: 'Your second name'
+    }
+  },
+  email: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+    label: "E-mail address",
+    optional: false,
+    autoform: {
+      placeholder: 'Your email'
+    }
+  },
+  company: {
+    type: String,
+    label: "Company / Organization / Educational institution",
+    max: 50,
+    optional: false,
+    autoform: {
+      placeholder: 'Company / Organization / Educational institution'
+    }
+  },
+  diet: {
+    type: String,
+    label: "Special diet",
+    max: 1000,
+    optional: true,
+    autoform: {
+      rows: 5,
+      placeholder: 'Special diet'
+    }
+  },
+  teamMembers: {
+    type: String,
+    label: "Other team members",
+    max: 1000,
+    optional: false,
+    autoform: {
+      rows: 5,
+      placeholder: 'Provide name, company/organization/educational institution, email (max 3 persons per team)'
+    }
+  },
+  teamName: {
+    type: String,
+    label: "Name of the hackathon team",
+    max: 1000,
+    optional: false,
+    autoform: {
+      rows: 5,
+      placeholder: 'Name of the hackathon team'
+    }
+  },
+  teamInfo: {
+    type: String,
+    label: "Tell us about your team",
+    max: 1000,
+    optional: false,
+    autoform: {
+      rows: 5,
+      placeholder: 'Tell us about your team'
+    }
+  },
+  teamIdeas: {
+    type: String,
+    label: "Tell us about the type of problem you want to solve or idea you have",
+    max: 1000,
+    optional: false,
+    autoform: {
+      rows: 5,
+      placeholder: 'Tell us about the type of problem you want to solve or idea you have'
+    }
+  },
+});

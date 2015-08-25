8befa051313373dc4bb44cd8c42e3b93c1b89203
elnzv <illya.nizyev@gmail.com>
2015-08-25 15:34:12 +0300

    method to send confirmation emails

--- server/methods/registerForm.js
+++ server/methods/registerForm.js
@@ -2,12 +2,18 @@ Meteor.methods({
   register: function(registrationId) {
     var registration = Registrations.findOne(registrationId);
 
-    // Send the e-mail
+    // Send the e-mail to organizers
     Email.send({
       to: Meteor.settings.mail.masterEmail,
       from: registration.email,
       subject: "Apinf Hackathon Register Form - Message From " + registration.firstName + registration.secondName,
       html: Handlebars.templates['registrationContent'](registration)
+    });
+    Email.send({
+      to: registration.email,
+      from: Meteor.settings.mail.masterEmail,
+      subject: "Apinf Hackathon Registrations",
+      html: Handlebars.templates['confirmationLetter'](registration)
     });
   }
 });
